3ee705fca179f0daf8b0aafa3af253b0be0b3f85
Jared Ning <jared@redningja.com>
2012-02-01 23:17:16 -0600

    Find dark knight only if cron has new accounts.

--- cron.rb
+++ cron.rb
@@ -1,11 +1,13 @@
 new_accounts = TwitterAccount.create_from_followers
 
-# Have each new account track The Dark Knight.
-dark_knight_imax = MovieTicketsMovie.find_by_title!('The Dark Knight Rises: The IMAX Experience')
-dark_knight      = MovieTicketsMovie.find_by_title!('The Dark Knight Rises')
-new_accounts.each do |account|
-  account.movie_tickets_movies << dark_knight_imax
-  account.movie_tickets_movies << dark_knight
+if new_accounts.any?
+  # Have each new account track The Dark Knight.
+  dark_knight_imax = MovieTicketsMovie.find_by_title!('The Dark Knight Rises: The IMAX Experience')
+  dark_knight      = MovieTicketsMovie.find_by_title!('The Dark Knight Rises')
+  new_accounts.each do |account|
+    account.movie_tickets_movies << dark_knight_imax
+    account.movie_tickets_movies << dark_knight
+  end
 end
 
 new_followings    = TwitterAccount.follow_all_not_followed
